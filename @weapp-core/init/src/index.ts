def6840fe5df83df0cff61a94f534cfe964d7b0f
ice breaker <1324318532@qq.com>
2024-09-19 20:41:25 +0800

    feat: improve @weapp-core/init

--- @weapp-core/init/src/index.ts
+++ @weapp-core/init/src/index.ts
@@ -3,15 +3,24 @@ import type { ProjectConfig, SharedUpdateOptions, UpdatePackageJsonOptions, Upda
 import path from 'node:path'
 import process from 'node:process'
 import logger from '@weapp-core/logger'
-import { get, set } from '@weapp-core/shared'
+import { defu, get, set } from '@weapp-core/shared'
 import fs from 'fs-extra'
 import { createContext } from './context'
 
 const ctx = createContext()
 
 export function updateProjectConfig(options: UpdateProjectConfigOptions) {
-  const { root, dest, cb, write = true } = options
-  const projectConfigFilename = ctx.projectConfig.name = 'project.config.json'
+  const { root, dest, cb, write, filename } = defu<
+    Required<UpdateProjectConfigOptions>,
+    Partial<UpdateProjectConfigOptions>[]
+  >(
+    options,
+    {
+      write: true,
+      filename: 'project.config.json',
+    },
+  )
+  const projectConfigFilename = ctx.projectConfig.name = filename
   const projectConfigPath = ctx.projectConfig.path = path.resolve(root, projectConfigFilename)
   if (fs.existsSync(projectConfigPath)) {
     try {
@@ -63,36 +72,50 @@ export function updateProjectConfig(options: UpdateProjectConfigOptions) {
 }
 
 export function updatePackageJson(options: UpdatePackageJsonOptions) {
-  const { root, dest, command, cb, write = true } = options
-  const packageJsonFilename = ctx.packageJson.name = 'package.json'
+  const { root, dest, command, cb, write, filename } = defu<
+    Required<UpdatePackageJsonOptions>,
+    Partial<UpdatePackageJsonOptions>[]
+  >(options, {
+    write: true,
+    filename: 'package.json',
+    command: 'weapp-vite',
+  })
+  const packageJsonFilename = ctx.packageJson.name = filename
   const packageJsonPath = ctx.packageJson.path = path.resolve(root, packageJsonFilename)
+  let packageJson: PackageJson
   if (fs.existsSync(packageJsonPath)) {
-    try {
-      const packageJson = fs.readJSONSync(packageJsonPath) as PackageJson
-      set(packageJson, 'scripts.dev', `${command} dev`)
-      set(packageJson, 'scripts.build', `${command} build`)
-      if (command === 'weapp-vite') {
-        // set(packageJson, 'type', 'module')
-        set(packageJson, 'scripts.open', `${command} open`)
-        set(packageJson, 'scripts.build-npm', `${command} build-npm`)
-        set(packageJson, 'devDependencies.miniprogram-api-typings', `latest`)
-      }
-      cb?.(
-        (...args) => {
-          set(packageJson, ...args)
-        },
-      )
-      if (write) {
-        fs.outputJSONSync(dest ?? packageJsonPath, packageJson, {
-          spaces: 2,
-        })
-        logger.log(`✨ 设置 ${packageJsonFilename} 配置文件成功!`)
-      }
-      ctx.packageJson.value = packageJson
-      return packageJson
-    }
-    catch { }
+    packageJson = fs.readJSONSync(packageJsonPath) as PackageJson
   }
+  else {
+    packageJson = {
+      name: 'weapp-vite-app',
+    }
+  }
+  try {
+    set(packageJson, 'scripts.dev', `${command} dev`)
+    set(packageJson, 'scripts.build', `${command} build`)
+    if (command === 'weapp-vite') {
+      // set(packageJson, 'type', 'module')
+      set(packageJson, 'scripts.open', `${command} open`)
+      set(packageJson, 'scripts.build-npm', `${command} build-npm`)
+      set(packageJson, 'devDependencies.miniprogram-api-typings', `latest`)
+      set(packageJson, 'devDependencies.weapp-vite', `latest`)
+    }
+    cb?.(
+      (...args) => {
+        set(packageJson, ...args)
+      },
+    )
+    if (write) {
+      fs.outputJSONSync(dest ?? packageJsonPath, packageJson, {
+        spaces: 2,
+      })
+      logger.log(`✨ 设置 ${packageJsonFilename} 配置文件成功!`)
+    }
+    ctx.packageJson.value = packageJson
+    return packageJson
+  }
+  catch { }
 }
 
 export function initViteConfigFile(options: SharedUpdateOptions) {
@@ -219,11 +242,57 @@ export function initTsJsonFiles(options: SharedUpdateOptions) {
   }
 }
 
+function updateGitIgnore(options: SharedUpdateOptions) {
+  const { root, write = true } = options
+  const filepath = path.resolve(root, '.gitignore')
+  const data = `# dependencies
+node_modules
+.pnp
+.pnp.js
+
+# testing
+coverage
+
+# next.js
+.next/
+out/
+build
+
+# misc
+.DS_Store
+*.pem
+
+# debug
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+.pnpm-debug.log*
+
+# local env files
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+# turbo
+.turbo
+
+dist
+vite.config.ts.timestamp-*.mjs`
+  if (write) {
+    fs.outputFileSync(filepath, data, {
+      encoding: 'utf8',
+    })
+  }
+  return data
+}
+
 export function initConfig(options: { root?: string, command?: 'weapp-vite' }) {
   const { root = process.cwd(), command } = options
 
   updateProjectConfig({ root })
   updatePackageJson({ root, command })
+  updateGitIgnore({ root })
   if (command === 'weapp-vite') {
     initViteConfigFile({ root })
     initTsDtsFile({ root })
