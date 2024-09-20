f9b50be947bcf4e8991c4c9e2e1ce5db72fd33b8
ice breaker <1324318532@qq.com>
2024-09-20 17:23:39 +0800

    chore: bump version

---
+++ .changeset/itchy-impalas-listen.md
@@ -0,0 +1,5 @@
+---
+"weapp-vite": patch
+---
+
+fix: dist watch 目录无限死循环问题
--- apps/vite-native-skyline/vite.config.ts
+++ apps/vite-native-skyline/vite.config.ts
@@ -1,3 +1,10 @@
+import { UnifiedViteWeappTailwindcssPlugin as uvwt } from 'weapp-tailwindcss/vite'
 import { defineConfig } from 'weapp-vite/config'
 
-export default defineConfig({})
+export default defineConfig({
+  plugins: [
+    uvwt({
+      rem2rpx: true,
+    }),
+  ],
+})
