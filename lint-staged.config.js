ae95c20c4907cdae8d3720a3d7d257f13b7a61a9
ice breaker <1324318532@qq.com>
2024-09-24 23:35:54 +0800

    chore(deps): upgrade

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 export default {
-  '*.{js,jsx,mjs,ts,tsx,mts,vue}': [
+  '!(apps)/**/*.{js,jsx,mjs,ts,tsx,mts,vue}': [
     'eslint --fix',
   ],
-  '*.{json,md,mdx,css,html,yml,yaml,scss}': [
+  '!(apps)/**/*.{json,md,mdx,css,html,yml,yaml,scss}': [
     // 'prettier --with-node-modules --ignore-path .prettierignore --write',
     'eslint --fix',
   ],
