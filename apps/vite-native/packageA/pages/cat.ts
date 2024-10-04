bd2b47f7f707c4dde57a2f5533233b2e707aff13
ice breaker <1324318532@qq.com>
2024-10-04 16:27:52 +0800

    docs: 添加 npm 自动构建章节

--- apps/vite-native/packageA/pages/cat.ts
+++ apps/vite-native/packageA/pages/cat.ts
@@ -1,5 +1,11 @@
-console.log('cat')
+import { add as add0 } from 'lodash'
+import { add as add1 } from 'lodash-es'
 
 Page({
-
+  data: {
+    num0: add0(1, 1),
+    num1: add1(2, 2),
+  },
 })
+
+console.log('cat')
