525830ea9715e1e7934b81e16ae6aaba93f6a4c7
ice breaker <1324318532@qq.com>
2024-10-07 18:26:03 +0800

    chore: init @weapp-core/schematics project

---
+++ @weapp-core/schematics/src/json.ts
@@ -0,0 +1,11 @@
+// import { z } from 'zod'
+// import { zodToJsonSchema } from 'zod-to-json-schema'
+
+// const mySchema = z
+//   .object({
+//     myString: z.string().min(5),
+//     myUnion: z.union([z.number(), z.boolean()]),
+//   })
+//   .describe('My neat object schema')
+
+// const jsonSchema = zodToJsonSchema(mySchema)
