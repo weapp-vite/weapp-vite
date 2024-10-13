9d78c10bbbbda23f09d854f5948a1c558387b86a
ice breaker <1324318532@qq.com>
2024-10-13 22:55:02 +0800

    docs: add opensource dashboard

--- website/.vitepress/theme/index.ts
+++ website/.vitepress/theme/index.ts
@@ -4,10 +4,12 @@ import TwoslashFloatingVue from '@shikijs/vitepress-twoslash/client'
 import Theme from 'vitepress/theme'
 import '@shikijs/vitepress-twoslash/style.css'
 import './index.scss'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 export default {
   extends: Theme,
   enhanceApp({ app }: EnhanceAppContext) {
+    // @ts-ignore
     app.use(TwoslashFloatingVue)
   },
 }
---
+++ website/dashboard.md
@@ -0,0 +1,9 @@
+---
+layout: page
+---
+
+<script setup lang="ts">
+import Dashboard from './.vitepress/components/Dashboard.vue'
+</script>
+
+<Dashboard></Dashboard>
