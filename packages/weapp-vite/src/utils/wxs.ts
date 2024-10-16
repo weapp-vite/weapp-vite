05287c33d0e500c4f71600dfea5d32ff4489111f
ice breaker <1324318532@qq.com>
2024-10-17 01:13:48 +0800

    feat: build for wxs

--- packages/weapp-vite/src/utils/wxs.ts
+++ packages/weapp-vite/src/utils/wxs.ts
@@ -1,8 +1,6 @@
 import type { Plugin } from 'esbuild'
 import type { TsupOptions } from '../types'
 import { defu } from '@weapp-core/shared'
-import fs from 'fs-extra'
-// 创建自定义 esbuild 插件
 
 // 重命名逻辑：将 .wxs.ts 重命名为 .wxs
 function renameCallback(oldPath: string) {
@@ -12,12 +10,8 @@ function renameCallback(oldPath: string) {
 const RenamePlugin: Plugin = {
   name: 'rename-output-files',
   setup(build) {
-    console.log(build)
-    build.onStart(() => {
-      console.log('------------')
-    })
     // 使用 onEnd 钩子，在构建完成时执行
-    build.onEnd(async (result) => {
+    build.onEnd((result) => {
       // 访问构建产物的文件路径
       if (result.outputFiles) {
         for (const output of result.outputFiles) {
@@ -25,9 +19,7 @@ const RenamePlugin: Plugin = {
           const newPath = renameCallback(oldPath) // 通过回调函数获取新的文件名
 
           if (oldPath !== newPath) {
-            // 使用 Node.js 的 fs 模块进行重命名
-            await fs.rename(oldPath, newPath)
-            // console.log(`Renamed: ${oldPath} -> ${newPath}`)
+            output.path = newPath
           }
         }
       }
@@ -41,21 +33,24 @@ export async function buildWxs(options: TsupOptions & { outbase?: string }) {
   const mergedOptions: TsupOptions = defu<TsupOptions, TsupOptions[]>(options, {
     format: 'cjs',
     target: 'es5',
-    // silent: true,
+    silent: true,
     shims: true,
     loader: {
       '.wxs': 'js',
     },
+
     esbuildOptions: (opts) => {
       opts.entryNames = '[dir]/[name]'
       opts.outExtension = {
         '.js': '.wxs',
       }
       opts.outbase = options.outbase
+      // opts.minify = true
     },
+
     sourcemap: false,
     config: false,
-    plugins: [RenamePlugin],
+    esbuildPlugins: [RenamePlugin],
   })
   await tsupBuild(mergedOptions)
 }
