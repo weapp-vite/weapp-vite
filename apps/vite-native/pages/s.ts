2425f6ee8889c451aa549dcd3677cea600d65618
ice breaker <1324318532@qq.com>
2024-10-18 23:11:13 +0800

    docs: update json-enhance

---
+++ apps/vite-native/pages/s.ts
@@ -0,0 +1,7 @@
+import type { Component } from 'weapp-vite/json'
+
+export default <Component>{
+  component: true,
+  styleIsolation: 'apply-shared',
+  usingComponents: {},
+}
---
+++ apps/vite-native/theme.json.ts
@@ -0,0 +1,14 @@
+import { defineThemeJson } from 'weapp-vite/json'
+
+export default defineThemeJson(
+  {
+    light: {
+      navBgColor: '#f6f6f6',
+      navTxtStyle: 'black',
+    },
+    dark: {
+      navBgColor: '#191919',
+      navTxtStyle: 'white',
+    },
+  },
+)
