196e402769374dc71aed8fd38d266d1e737d12fe
ice breaker <1324318532@qq.com>
2024-10-18 22:31:39 +0800

    chore(deps): commit code

---
+++ apps/vite-native/components/navigation-bar/navigation-bar.json.ts
@@ -0,0 +1,9 @@
+import { defineComponentJson } from 'weapp-vite/json'
+
+export default defineComponentJson(
+  {
+    component: true,
+    styleIsolation: 'apply-shared',
+    usingComponents: {},
+  },
+)
