6fed854269d7ecd444aacf7c8b360252ffe2feac
ice breaker <1324318532@qq.com>
2024-10-29 23:46:33 +0800

    chore: commit module global

--- apps/vite-native/pages/index/index.json.ts
+++ apps/vite-native/pages/index/index.json.ts
@@ -1,10 +1,11 @@
 import type { Page } from 'weapp-vite/json'
 import fs from 'node:fs/promises'
 import path from 'node:path'
+import process from 'node:process'
 import xxx from '@/assets/share'
 import shared from './shared.json'
 
-console.log(import.meta.env, import.meta.dirname)// , __dirname)
+console.log(import.meta.env, import.meta.dirname, process.env.MP_PLATFORM, process.env.TZ)// , __dirname)
 const key = await fs.readFile(path.resolve(import.meta.dirname, 'x.txt'), 'utf8')
 console.log(key)
 export default <Page>{
--- packages/weapp-vite/client.d.ts
+++ packages/weapp-vite/client.d.ts
@@ -1,7 +1,18 @@
 /// <reference types="vite/client" />
 
+type MP_PLATFORM = 'weapp'
+
 interface ImportMetaEnv {
-  MP_PLATFORM: 'weapp' // | 'swan' | 'alipay' | 'tt' | 'qq' | 'jd' | 'h5'
+  MP_PLATFORM: MP_PLATFORM // | 'swan' | 'alipay' | 'tt' | 'qq' | 'jd' | 'h5'
 }
 // MP_PLATFORM
 // weapp / swan / alipay / tt / qq / jd / h5
+declare module 'process' {
+  global {
+    namespace NodeJS {
+      interface ProcessEnv extends Dict<string> {
+        MP_PLATFORM: MP_PLATFORM
+      }
+    }
+  }
+}
