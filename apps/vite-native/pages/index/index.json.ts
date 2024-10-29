2cba68a9d2acf27cba6a2b34f3f30cbfd20812ed
ice breaker <1324318532@qq.com>
2024-10-29 23:04:53 +0800

    chore: support platform options

--- apps/vite-native/pages/index/index.json.ts
+++ apps/vite-native/pages/index/index.json.ts
@@ -2,6 +2,8 @@ import type { Page } from 'weapp-vite/json'
 import xxx from '@/assets/share'
 import shared from './shared.json'
 
+console.log(import.meta.env)
+
 export default <Page>{
   usingComponents: {
     't-button': 'tdesign-miniprogram/button/button',
