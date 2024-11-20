4a7319f6926a94348061efd3750a4e259cfb4d93
ice breaker <1324318532@qq.com>
2024-11-20 23:02:25 +0800

    fix: [Bug]: 使用 clsx 报错 #59

---
+++ .changeset/clean-pumas-unite.md
@@ -0,0 +1,6 @@
+---
+"weapp-vite": patch
+---
+
+fix: [#59](https://github.com/weapp-vite/weapp-vite/issues/59)
+feat: 优化清空目录的方式
--- apps/vite-native/pages/test/test.ts
+++ apps/vite-native/pages/test/test.ts
@@ -1,5 +1,12 @@
+import { clsx } from 'clsx'
 import x, { a } from './aaa'
 // const ccc = require('aaa')
-Page({})
+Page({
+  data: {
+    className: clsx({
+      'bg-[#45ea2c]': true,
+    }, 'h-[323.43px]', 'flex items-start justify-center'),
+  },
+})
 
 console.log(x, a)
