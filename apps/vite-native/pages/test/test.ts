1030a7e5fb585ca0022d84a259c07f0c88e4595b
ice breaker <1324318532@qq.com>
2024-11-20 23:43:12 +0800

    chore: add class-variance-authority as dep

--- apps/vite-native/pages/test/test.ts
+++ apps/vite-native/pages/test/test.ts
@@ -1,11 +1,51 @@
+import { cva } from 'class-variance-authority'
 import { clsx } from 'clsx'
 import x, { a } from './aaa'
-// const ccc = require('aaa')
+
+const button = cva(['font-semibold', 'border', 'rounded'], {
+  variants: {
+    intent: {
+      primary: [
+        'bg-blue-500',
+        'text-white',
+        'border-transparent',
+        'hover:bg-blue-600',
+      ],
+      // **or**
+      // primary: "bg-blue-500 text-white border-transparent hover:bg-blue-600",
+      secondary: [
+        'bg-white',
+        'text-gray-800',
+        'border-gray-400',
+        'hover:bg-gray-100',
+      ],
+    },
+    size: {
+      small: ['text-sm', 'py-1', 'px-2'],
+      medium: ['text-base', 'py-2', 'px-4'],
+    },
+  },
+  compoundVariants: [
+    {
+      intent: 'primary',
+      size: 'medium',
+      class: 'uppercase',
+      // **or** if you're a React.js user, `className` may feel more consistent:
+      // className: "uppercase"
+    },
+  ],
+  defaultVariants: {
+    intent: 'primary',
+    size: 'medium',
+  },
+})
+
 Page({
   data: {
     className: clsx({
       'bg-[#45ea2c]': true,
     }, 'h-[323.43px]', 'flex items-start justify-center'),
+    buttonClass: button(),
   },
 })
 
