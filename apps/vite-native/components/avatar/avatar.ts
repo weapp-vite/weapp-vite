a9e1152cb2c99119a058acef6e0fd2ee3c712a40
ice breaker <1324318532@qq.com>
2024-11-28 22:49:53 +0800

    chore(deps): upgrade

--- apps/vite-native/components/avatar/avatar.ts
+++ apps/vite-native/components/avatar/avatar.ts
@@ -1,3 +1,10 @@
+import { Cos, a } from '@/utils/cos'
+console.log('-----------')
+console.log(a)
+console.log('-----------')
 Component({
-
+  data: {
+    Cos,
+    a
+  }
 })
--- apps/vite-native/pages/LoveFromChina/index.ts
+++ apps/vite-native/pages/LoveFromChina/index.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import Cos from 'cos-wx-sdk-v5'
 
-console.log('npm', Cos)
+console.log('npm', Object.keys(Cos), new Cos())
 
 Page({})
