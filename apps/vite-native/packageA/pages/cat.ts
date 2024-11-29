e32eb3fcc331520b8260769a57fa4463bd68080f
ice breaker <1324318532@qq.com>
2024-11-29 22:30:20 +0800

    chore: add mini-stores as demo

--- apps/vite-native/packageA/pages/cat.ts
+++ apps/vite-native/packageA/pages/cat.ts
@@ -1,13 +1,22 @@
+// import Cos from '@/utils/cos-wx-sdk-v5'
+import globalStore from '@/stores/index'
 import { formatTime } from '@/utils/util'
 import { add as add0 } from 'lodash'
 import { add as add1 } from 'lodash-es'
-import Cos from '@/utils/cos-wx-sdk-v5'
+
 Page({
   data: {
     num0: add0(1, 1),
     num1: add1(2, 2),
     now: formatTime(new Date()),
-    Cos
+    // Cos
+  },
+  onLoad(query) {
+    globalStore.bind(this, '$data')
+  },
+  handleChangeTitle() {
+    globalStore.data.title = '新标题'
+    globalStore.update()
   },
 })
 
