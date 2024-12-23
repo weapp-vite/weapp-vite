0ba1ff3e66d04f266d981c7f76337671e073a09e
ice breaker <1324318532@qq.com>
2024-12-23 10:53:37 +0800

    fix: [Bug]: autoImportComponents没有起作用

--- apps/vite-native-ts/vite.config.ts
+++ apps/vite-native-ts/vite.config.ts
@@ -4,6 +4,16 @@ export default defineConfig({
   weapp: {
     // weapp-vite options
     srcRoot: './miniprogram',
-    platform: 'alipay',
+    // platform: 'alipay',
+    enhance: {
+      autoImportComponents: {
+        globs: ['miniprogram/components/**/*']
+      }
+    },
+    generate: {
+      dirs: {
+        component: './miniprogram/components'
+      }
+    }
   },
 })
