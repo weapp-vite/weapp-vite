cd15d24428da44231dc35c94169cdc7b3dd5b6de
ice breaker <1324318532@qq.com>
2024-12-28 15:09:16 +0800

    chore: change env to weapp-vite-dev

--- apps/vite-native/vite.config.ts
+++ apps/vite-native/vite.config.ts
@@ -1,15 +1,14 @@
-/* eslint-disable import/first */
-// eslint-disable-next-line import/newline-after-import
-import { register } from 'tsx/esm/api'
-register()
 import type { UserConfig } from 'weapp-vite/config'
 import path from 'node:path'
 import process from 'node:process'
-
 // import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
-import Inspect from 'vite-plugin-inspect'
+// import Inspect from 'vite-plugin-inspect'
+
 import { UnifiedViteWeappTailwindcssPlugin as uvwt } from 'weapp-tailwindcss/vite'
 import { TDesignResolver, VantResolver } from 'weapp-vite/auto-import-components/resolvers'
+// import { register } from 'tsx/esm/api'
+// register()
+// import 'jiti/register'
 
 // await import('tsx/esm/api').then(({ register }) => {
 //   register()
--- packages/weapp-vite/dev/bin.js
+++ packages/weapp-vite/dev/bin.js
@@ -1,2 +1,15 @@
-#!/usr/bin/env -S npx tsx
-import '../dist/cli.mjs'
+#!/usr/bin/env node
+
+// import { createJiti } from 'jiti'
+
+// const jiti = createJiti(import.meta.url, {
+//   transformModules: [
+//     '@weapp-core/shared',
+//   ],
+// })
+
+// await jiti.import('../dist/cli.mjs')
+
+// import 'jiti/register'
+// import '../dist/cli.mjs'
+// await import('importx').then(x => x.import('../dist/cli.mjs', import.meta.url))
