6e5414249a073254c4da1096a783a3da20004db8
ice breaker <1324318532@qq.com>
2025-02-11 22:13:34 +0800

    chore(deps): upgrade

--- apps/vite-native/pages/index/test.ts
+++ apps/vite-native/pages/index/test.ts
@@ -1,12 +1,12 @@
-import { fuck } from 'what/fuck'
-import { the } from 'what/the'
+// import { fuck } from 'what/fuck'
+// import { the } from 'what/the'
 import { fuck as fuck2 } from '../../shit/fuck'
 import { the as the2 } from '../../shit/the'
-import { fuck as fuck1 } from '/shit/fuck'
-import { the as the1 } from '/shit/the'
+// import { fuck as fuck1 } from '/shit/fuck'
+// import { the as the1 } from '/shit/the'
 
-console.log(fuck, the)
-console.log(the1, fuck1)
+// console.log(fuck, the)
+// console.log(the1, fuck1)
 console.log(the2, fuck2)
 Page({
   data: {
