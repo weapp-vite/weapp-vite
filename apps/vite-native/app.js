3389fd7c30ae6455c68879503bedc751e1a98024
ice breaker <1324318532@qq.com>
2025-03-11 01:09:14 +0800

    chore: try virtualModule

--- apps/vite-native/app.js
+++ apps/vite-native/app.js
@@ -1,9 +1,9 @@
-import { cn } from '@weapp-tailwindcss/merge'
+import { twMerge } from '@weapp-tailwindcss/merge'
 import dayjs from 'dayjs'
 import { add } from 'lodash'
 
-console.log(cn('weapp-reset-button px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80', 'bg-[blue]'))
-const { formatTime } = require('./utils/util')
+console.log(twMerge('weapp-reset-button px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80', 'bg-[blue]'))
+const { formatTime } = require('./utils/util.cjs')
 
 console.log(add(1, 2))
 console.log(dayjs())
