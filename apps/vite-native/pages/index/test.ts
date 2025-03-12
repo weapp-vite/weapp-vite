6d686bd5f5e199f6867408ca995bdcb0eafccc39
ice breaker <1324318532@qq.com>
2025-03-13 01:39:55 +0800

    chore(deps): upgrade

--- apps/vite-native/pages/index/test.ts
+++ apps/vite-native/pages/index/test.ts
@@ -1,16 +1,33 @@
+// import { fuck as fuck1 } from '/shit/fuck'
+// import { the as the1 } from '/shit/the'
+import { ComponentWithComputed } from 'miniprogram-computed'
 // import { fuck } from 'what/fuck'
 // import { the } from 'what/the'
 import { fuck as fuck2 } from '../../shit/fuck'
 import { the as the2 } from '../../shit/the'
-// import { fuck as fuck1 } from '/shit/fuck'
-// import { the as the1 } from '/shit/the'
-
 // console.log(fuck, the)
 // console.log(the1, fuck1)
 console.log(the2, fuck2)
-Page({
+ComponentWithComputed({
   data: {
     className: '',
     matterList: [1, 2, 3, 4],
+    a: 1,
+    b: 1,
+  },
+  computed: {
+    sum(data) {
+      // 注意： computed 函数中不能访问 this ，只有 data 对象可供访问
+      // 这个函数的返回值会被设置到 this.data.sum 字段中
+      return data.a + data.b // data.c 为自定义 behavior 数据段
+    },
+  },
+  methods: {
+    onClick() {
+      console.log('click-----')
+      this.setData({
+        a: this.data.a + 1,
+      })
+    },
   },
 })
