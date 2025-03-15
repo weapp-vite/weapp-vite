56f13e79f6ac2190ab7c2fa89aacda8ea106bb2f
ice breaker <1324318532@qq.com>
2025-03-16 00:25:05 +0800

    chore: bump version

--- packages/weapp-vite/src/plugins/index.ts
+++ packages/weapp-vite/src/plugins/index.ts
@@ -1,5 +1,5 @@
 import type { CompilerContext } from '@/context'
-import type { SubPackageMetaValue } from '@/types'
+import type { SubPackageMetaValue, WeappVitePluginApi } from '@/types'
 import type { Plugin } from 'vite'
 import { VitePluginService } from './VitePluginService'
 
@@ -10,13 +10,22 @@ import { VitePluginService } from './VitePluginService'
 
 // https://github.com/rollup/rollup/blob/c6751ff66d33bf0f4c87508765abb996f1dd5bbe/src/watch/fileWatcher.ts#L2
 // https://github.com/rollup/rollup/blob/c6751ff66d33bf0f4c87508765abb996f1dd5bbe/src/watch/watch.ts#L174
-export function vitePluginWeapp(ctx: CompilerContext, subPackageMeta?: SubPackageMetaValue): Plugin[] {
-  const service = new VitePluginService(ctx)
 
+export function vitePluginWeapp(ctx: CompilerContext, subPackageMeta?: SubPackageMetaValue): Plugin<WeappVitePluginApi>[] {
+  const service = new VitePluginService(ctx)
+  const api = {
+    get ctx() {
+      return ctx
+    },
+    get service() {
+      return service
+    },
+  }
   return [
     {
       name: 'weapp-vite:pre',
       enforce: 'pre',
+      api,
       // config->configResolved->|watching|options->buildStart
       // config(config, env) {
       //   debug?.(config, env)
@@ -54,12 +63,14 @@ export function vitePluginWeapp(ctx: CompilerContext, subPackageMeta?: SubPackag
     {
       // todo
       name: 'weapp-vite',
+      api,
       // https://github.com/vitejs/vite/blob/3400a5e258a597499c0f0808c8fca4d92eeabc17/packages/vite/src/node/plugins/css.ts#L6
     },
     {
       // todo
       name: 'weapp-vite:post',
       enforce: 'post',
+      api,
     },
   ]
 }
--- packages/weapp-vite/src/types/index.ts
+++ packages/weapp-vite/src/types/index.ts
@@ -1,3 +1,4 @@
 export * from './config'
 export * from './context'
 export * from './entry'
+export * from './plugin'
