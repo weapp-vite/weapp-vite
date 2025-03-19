52cc3802694d567a4803f988ddaf95fd576baed9
ice breaker <1324318532@qq.com>
2025-03-19 15:12:10 +0800

    chore: deps upgrade

--- apps/ast/src/pages/index/index.ts
+++ apps/ast/src/pages/index/index.ts
@@ -1,5 +1,12 @@
+import { x } from '../../utils'
+// @ts-ignore
+// eslint-disable-next-line ts/no-require-imports
+const y = require('../../utils/async')
+
 Page(
   {
-
+    onReady() {
+      console.log(x, y)
+    },
   },
 )
