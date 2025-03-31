e5830522ba086959ca5632a58e1d077a99ee0c56
ice breaker <1324318532@qq.com>
2025-04-01 01:02:03 +0800

    chore: commit rltps

--- packages/weapp-vite/test/demo/next.test.ts
+++ packages/weapp-vite/test/demo/next.test.ts
@@ -1,13 +1,17 @@
-/* eslint-disable ts/no-unused-vars */
 import type { CompilerContext } from '@/context'
+import type { Entry } from '@/types'
+import type { App, Component, Page } from '@weapp-core/schematics'
+import type { PluginContext } from 'rollup'
 import type { Plugin, ResolvedConfig } from 'vite'
 import { createCompilerContext } from '@/createContext'
 import { changeFileExtension, findJsonEntry } from '@/utils/file'
 import { jsonFileRemoveJsExtension, stringifyJson } from '@/utils/json'
-import { } from '@weapp-core/shared'
+import { removeExtension } from '@weapp-core/shared'
 import fs from 'fs-extra'
 import path from 'pathe'
 import { build } from 'vite'
+import commonjs from 'vite-plugin-commonjs'
+import tsconfigPaths from 'vite-tsconfig-paths'
 // import { pages } from '../../../../apps/vite-native/app.json'
 
 // console.log(pages)
@@ -17,6 +21,7 @@ import { build } from 'vite'
 // const resolvedVirtualModuleId = `\0${virtualModuleId}`
 const removePlugins = ['vite:build-import-analysis']
 
+// eslint-disable-next-line ts/no-unused-vars
 const FIND_MAP = {
   app: {
     json: {
@@ -63,12 +68,32 @@ const FIND_MAP = {
 }
 
 function weappVite(ctx: CompilerContext): Plugin[] {
+  const entriesMap = new Map<string, Entry | undefined>()
+  // eslint-disable-next-line ts/no-unused-vars
+  let resolvedConfig: ResolvedConfig
+  function emitEntriesChunks(this: PluginContext, entries: string[]) {
+    return entries.map(async (x) => {
+      const absPath = path.resolve(ctx.configService.absoluteSrcRoot, x)
+      const resolvedId = await this.resolve(absPath)
+      if (resolvedId) {
+        await this.load(resolvedId)
+        const fileName = ctx.configService.relativeAbsoluteSrcRoot(changeFileExtension(resolvedId.id, '.js'))
+        this.emitFile(
+          {
+            type: 'chunk',
+            id: resolvedId.id,
+            fileName,
+          },
+        )
+      }
+    })
+  }
   return [
     {
       name: 'test',
-      async config(config, env) {
+      // async config(config, env) {
 
-      },
+      // },
       async configResolved(config: ResolvedConfig) {
         for (const removePlugin of removePlugins) {
           const idx = config.plugins?.findIndex(x => x.name === removePlugin)
@@ -76,42 +101,73 @@ function weappVite(ctx: CompilerContext): Plugin[] {
             (config.plugins as Plugin[]).splice(idx, 1)
           }
         }
+        resolvedConfig = config
       },
       resolveId(id) {
-
+        console.log('resolveId', id)
       },
       async load(id) {
-        // App
-        if ([
-          'app.js',
-          'app.ts',
-        ].includes(
+        const rltps = removeExtension(
           ctx
             .configService
             .relativeAbsoluteSrcRoot(id),
-        )) {
+        )
+        console.log('load', rltps)
+        if (entriesMap.has(rltps)) {
           const p = await findJsonEntry(id)
           if (p) {
-            const json = await ctx.jsonService.read(p)
-            const pages = json.pages as string[]
+            const json: Page | Component = await ctx.jsonService.read(p)
+
+            const components = Object.values(json.usingComponents ?? {})
+
+            const entries = components
+            for (const entry of entries) {
+              entriesMap.set(entry, undefined)
+            }
 
             await Promise.all(
-              pages.map((x) => {
-                return path.resolve(ctx.configService.absoluteSrcRoot, x)
-              }).map(async (x) => {
-                const resolvedId = await this.resolve(x)
-                if (resolvedId) {
-                  await this.load(resolvedId)
-                  const fileName = ctx.configService.relativeAbsoluteSrcRoot(changeFileExtension(resolvedId.id, '.js'))
-                  this.emitFile(
-                    {
-                      type: 'chunk',
-                      id: resolvedId.id,
-                      fileName,
-                    },
-                  )
-                }
-              }),
+              [
+                ...emitEntriesChunks.call(this, entries),
+              ],
+            )
+
+            this.emitFile(
+              {
+                type: 'asset',
+                fileName: ctx.configService.relativeAbsoluteSrcRoot(jsonFileRemoveJsExtension(p)),
+                source: stringifyJson(json),
+              },
+            )
+          }
+        }
+        else if ([
+          'app',
+        ].includes(
+          rltps,
+        )) {
+          // isApp
+          const p = await findJsonEntry(id)
+          if (p) {
+            const json: App = await ctx.jsonService.read(p)
+            const pages = (json.pages as string[]) ?? []
+            const components = Object.values(json.usingComponents ?? {})
+            const subPackages = (
+              json.subPackages?.reduce<string[]>(
+                (acc, cur) => {
+                  acc.push(...(cur.pages ?? []).map(x => `${cur.root}/${x}`))
+                  return acc
+                },
+                [],
+              )) ?? []
+            const entries = [...pages, ...components, ...subPackages]
+            for (const entry of entries) {
+              entriesMap.set(entry, undefined)
+            }
+
+            await Promise.all(
+              [
+                ...emitEntriesChunks.call(this, entries),
+              ],
             )
 
             this.emitFile(
@@ -124,9 +180,9 @@ function weappVite(ctx: CompilerContext): Plugin[] {
           }
         }
       },
-      async transform(code, id) {
+      // async transform(code, id) {
 
-      },
+      // },
     },
   ]
 }
@@ -163,6 +219,8 @@ async function main() {
 
     plugins: [
       weappVite(ctx),
+      tsconfigPaths(),
+      commonjs(),
     ],
   })
   return {
