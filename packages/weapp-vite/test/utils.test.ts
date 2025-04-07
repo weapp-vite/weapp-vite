a9e5568c1d479f20a82e7049c989656783c2dafb
ice breaker <1324318532@qq.com>
2025-04-07 20:01:49 +0800

    chore!: breaking changes remove codes

--- packages/weapp-vite/test/utils.test.ts
+++ packages/weapp-vite/test/utils.test.ts
@@ -1,7 +1,5 @@
 import { getProjectConfig } from '@/utils'
 import { diff } from 'just-diff'
-import path from 'pathe'
-import { isAppRoot, searchAppEntry } from './legacy/utils'
 import { absDirs } from './utils'
 
 describe('utils', () => {
@@ -11,32 +9,6 @@ describe('utils', () => {
         await getProjectConfig(p, { ignorePrivate: true }),
         await getProjectConfig(p),
       )).toMatchSnapshot()
-    })
-  })
-
-  describe.skip('isAppRoot', () => {
-    it.each(absDirs)('$name', ({ path: p, name }) => {
-      if (name.includes('-ts')) {
-        expect(isAppRoot(path.resolve(p, 'miniprogram'))).toBe(true)
-      }
-      else {
-        expect(isAppRoot(p)).toBe(true)
-      }
-    })
-  })
-
-  describe.skip('searchAppEntry', () => {
-    it.each(absDirs)('$name', ({ path: p, name }) => {
-      if (name.includes('-ts')) {
-        expect(searchAppEntry({
-          root: path.resolve(p, 'miniprogram'),
-        })).toBeTruthy()
-      }
-      else {
-        expect(searchAppEntry({
-          root: p,
-        })).toBeTruthy()
-      }
     })
   })
 })
--- packages/weapp-vite/test/wxs.test.ts
+++ packages/weapp-vite/test/wxs.test.ts
@@ -1,71 +1,14 @@
 import { init, parse } from 'cjs-module-lexer'
-import path from 'pathe'
-import { buildWxs } from 'test/legacy/wxs'
-import { getApp } from './utils'
+// import path from 'pathe'
+// import { getApp } from './utils'
 
-const viteNativeRoot = getApp('vite-native')
+// const viteNativeRoot = getApp('vite-native')
 
-function r(...args: string[]) {
-  return path.resolve(viteNativeRoot, ...args)
-}
+// function r(...args: string[]) {
+//   return path.resolve(viteNativeRoot, ...args)
+// }
 
 describe.skipIf(true)('wxs', () => {
-  it('build wxs case 0', async () => {
-    await buildWxs({
-      entry: [
-        r('pages/index/index.wxs.ts'),
-      ],
-      outDir: path.resolve(__dirname, './fixtures/wxsCase0'),
-      outbase: viteNativeRoot,
-      clean: true,
-    })
-  })
-
-  it('build wxs case 1', async () => {
-    await buildWxs({
-      entry: [
-        r('pages/index/cjs.wxs'),
-      ],
-      outDir: path.resolve(__dirname, './fixtures/wxsCase1'),
-      outbase: viteNativeRoot,
-      clean: true,
-    })
-  })
-
-  it('build wxs case 4', async () => {
-    await buildWxs({
-      entry: [
-        r('pages/index/esm.wxs'),
-      ],
-      outDir: path.resolve(__dirname, './fixtures/wxsCase4'),
-      outbase: viteNativeRoot,
-      clean: true,
-    })
-  })
-
-  it('build wxs case 2', async () => {
-    await buildWxs({
-      entry: [
-        r('pages/index/test.ts'),
-      ],
-      outDir: path.resolve(__dirname, './fixtures/wxsCase2'),
-      tsconfig: r('tsconfig.json'),
-      outbase: viteNativeRoot,
-      clean: true,
-    })
-  })
-
-  it('build wxs case 3', async () => {
-    await buildWxs({
-      entry: [
-        r('pages/index/index.wxs.js'),
-      ],
-      outDir: path.resolve(__dirname, './fixtures/wxsCase3'),
-      outbase: viteNativeRoot,
-      clean: true,
-    })
-  })
-
   it('code parse', async () => {
     await init()
     const code = `var tools = require("./tools.wxs");
