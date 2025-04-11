5a3c31c40a98d7e54732535d8f06dc99e0daa5da
ice breaker <1324318532@qq.com>
2025-04-11 10:48:54 +0800

    chore: assets fixture commit

---
+++ packages/weapp-vite/test/asset.test.ts
@@ -0,0 +1,32 @@
+import { createCompilerContext } from '@/createContext'
+import { cssCodeCache } from '@/plugins/css'
+import { wxsCodeCache } from '@/plugins/wxs'
+import CI from 'ci-info'
+import fs from 'fs-extra'
+import path from 'pathe'
+import { getFixture, scanFiles } from './utils'
+
+describe.skipIf(CI.isCI)('asset', () => {
+  const cwd = getFixture('asset')
+  const distDir = path.resolve(cwd, 'dist')
+  it('scanFiles', async () => {
+    await fs.remove(distDir)
+    const ctx = await createCompilerContext({
+      cwd,
+    })
+    await ctx.buildService.build()
+    expect(await fs.exists(distDir)).toBe(true)
+    const files = await scanFiles(distDir)
+    expect(files).toMatchSnapshot()
+    expect(wxsCodeCache.size).toBe(0)
+    expect(cssCodeCache.size).toBe(4)
+
+    expect(ctx.scanService.subPackageMetas).toMatchSnapshot()
+    expect(await fs.exists(path.resolve(distDir, 'miniprogram_npm'))).toBe(true)
+    expect(await fs.exists(path.resolve(distDir, 'miniprogram_npm/buffer'))).toBe(true)
+    expect(await fs.exists(path.resolve(distDir, 'miniprogram_npm/gm-crypto'))).toBe(true)
+    expect(await fs.exists(path.resolve(distDir, 'packageB/miniprogram_npm'))).toBe(true)
+    expect(await fs.exists(path.resolve(distDir, 'packageB/miniprogram_npm/buffer'))).toBe(true)
+    expect(await fs.exists(path.resolve(distDir, 'packageB/miniprogram_npm/gm-crypto'))).toBe(true)
+  })
+})
