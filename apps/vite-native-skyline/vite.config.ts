e19b07851ef43b6130401091eea8f51c42d5bb2d
ice breaker <1324318532@qq.com>
2025-04-25 16:36:01 +0800

    chore: fix type

--- apps/vite-native-skyline/vite.config.ts
+++ apps/vite-native-skyline/vite.config.ts
@@ -13,4 +13,21 @@ export default defineConfig({
       '@vant/weapp/common/index.css': path.resolve(__dirname, './node_modules/@vant/weapp/dist/common/index.wxss'),
     },
   },
+  weapp: {
+    debug: {
+      inspect: {
+        threshold: 100,
+        slient: true,
+        onHookExecution({ hookName, pluginName, duration, args }) {
+          console.log(`[${pluginName}] ${hookName.padEnd(20)} ⏱ ${duration.toFixed(2).padStart(6)} ms`)
+          if (hookName === 'transform') {
+            console.log(args[1])
+          }
+          else if (hookName === 'load') {
+            console.log(args[0])
+          }
+        },
+      },
+    },
+  },
 })
