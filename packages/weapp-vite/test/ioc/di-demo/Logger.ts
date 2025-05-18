b2f1523196b7f1e3ec46e5c0bfc1f056e8aa8167
ice breaker <1324318532@qq.com>
2025-05-18 23:43:49 +0800

    chore: commit logger

--- packages/weapp-vite/test/ioc/di-demo/Logger.ts
+++ packages/weapp-vite/test/ioc/di-demo/Logger.ts
@@ -2,6 +2,10 @@ import { injectable } from 'inversify'
 
 @injectable()
 export class Logger {
+  constructor() {
+    console.log('>> Created Logger')
+  }
+
   log(message: string) {
     console.log(`[LOG]: ${message}`)
   }
