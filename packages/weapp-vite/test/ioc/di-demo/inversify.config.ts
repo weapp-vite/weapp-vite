f0dbe9ab82166812d60fd0248f548c817ba9fdac
ice breaker <1324318532@qq.com>
2025-05-19 00:28:11 +0800

    chore: add request mode

--- packages/weapp-vite/test/ioc/di-demo/inversify.config.ts
+++ packages/weapp-vite/test/ioc/di-demo/inversify.config.ts
@@ -1,6 +1,8 @@
 import { Container } from 'inversify'
 import { Logger } from './Logger'
 import { ServiceA } from './ServiceA'
+import { ServiceAll } from './ServiceAll'
+import { ServiceB } from './ServiceB'
 import { TokenGenerator } from './TokenGenerator'
 import { TYPES } from './types'
 import { UserContext } from './UserContext'
@@ -11,5 +13,6 @@ container.bind<Logger>(TYPES.Logger).to(Logger).inSingletonScope()
 container.bind<UserContext>(TYPES.UserContext).to(UserContext).inRequestScope()
 container.bind<TokenGenerator>(TYPES.TokenGenerator).to(TokenGenerator).inTransientScope()
 container.bind<ServiceA>(TYPES.ServiceA).to(ServiceA)// .inSingletonScope()// .inRequestScope()
-
+container.bind<ServiceB>(TYPES.ServiceB).to(ServiceB)
+container.bind<ServiceAll>(TYPES.ServiceAll).to(ServiceAll)
 export { container }
