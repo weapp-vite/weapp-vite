28d9cf9225a664d760971ec2065d269772ad0ca6
ice breaker <1324318532@qq.com>
2025-05-21 18:10:06 +0800

    chore(deps): upgrade

--- packages/weapp-vite/src/inversify.config.ts
+++ packages/weapp-vite/src/inversify.config.ts
@@ -15,14 +15,16 @@ import { Symbols } from './context/Symbols'
 const container = new Container()
 
 container.bind<CompilerContext>(Symbols.CompilerContext).to(CompilerContext).inTransientScope()
-container.bind<JsonService>(Symbols.JsonService).to(JsonService).inRequestScope()
-container.bind<NpmService>(Symbols.NpmService).to(NpmService).inRequestScope()
+
 container.bind<WxmlService>(Symbols.WxmlService).to(WxmlService).inRequestScope()
-container.bind<ConfigService>(Symbols.ConfigService).to(ConfigService).inRequestScope()
 container.bind<WatcherService>(Symbols.WatcherService).to(WatcherService).inRequestScope()
 container.bind<ScanService>(Symbols.ScanService).to(ScanService).inRequestScope()
 container.bind<AutoImportService>(Symbols.AutoImportService).to(AutoImportService).inRequestScope()
 container.bind<BuildService>(Symbols.BuildService).to(BuildService).inRequestScope()
+
+container.bind<JsonService>(Symbols.JsonService).to(JsonService).inSingletonScope()
+container.bind<NpmService>(Symbols.NpmService).to(NpmService).inSingletonScope()
+container.bind<ConfigService>(Symbols.ConfigService).to(ConfigService).inSingletonScope()
 
 export {
   container,
