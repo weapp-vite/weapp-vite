644838f5ab5cd8df22401554ac0b9adc837558df
ice breaker <1324318532@qq.com>
2025-06-15 01:01:27 +0800

    fix: test case

--- packages/rolldown-require/test/esbuild.test.ts
+++ packages/rolldown-require/test/esbuild.test.ts
@@ -1,5 +1,5 @@
-import fs from 'node:fs'
-import path from 'node:path/posix'
+import fs from 'fs-extra'
+import path from 'pathe'
 import { assert } from 'vitest'
 import { bundleRequire, JS_EXT_RE } from './esbuild'
 
