df945272ea1779f48bc154aaf7997c2e0d75bf29
ice breaker <1324318532@qq.com>
2025-09-02 20:13:04 +0800

    chore: add vite build config

--- packages/experimental/test/fixtures/demo/_vite.config.ts
+++ packages/experimental/test/fixtures/demo/_vite.config.ts
@@ -3,6 +3,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig(() => {
   return {
-
+    define: {
+      'import.meta.env.VITE_APP_NAME': '"experimental"'
+    },
+    root: import.meta.dirname,
+    build: {
+      outDir: 'dist-build-config',
+      minify: true
+    }
   }
 })
