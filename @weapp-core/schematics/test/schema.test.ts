5c3f292cd17ca78914e6e902741f590b766d1a2c
ice breaker <1324318532@qq.com>
2025-10-02 04:38:14 +0800

    chore: fix test case

--- @weapp-core/schematics/test/schema.test.ts
+++ @weapp-core/schematics/test/schema.test.ts
@@ -1,7 +1,7 @@
-import { write2WebsiteFiles } from '../scripts/utils'
+import { buildSchemas } from '../scripts/utils'
 
 describe('schema', () => {
   it('should work', async () => {
-    await write2WebsiteFiles()
+    await buildSchemas()
   })
 })
