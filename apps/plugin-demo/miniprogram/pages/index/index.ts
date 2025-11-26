06bc30a3a0485513b7f1ae7e7faf2fdebd96b4af
ice breaker <1324318532@qq.com>
2025-11-27 01:07:17 +0800

    chore: typescript demo

--- apps/plugin-demo/miniprogram/pages/index/index.ts
+++ apps/plugin-demo/miniprogram/pages/index/index.ts
@@ -2,17 +2,18 @@ const plugin = requirePlugin('hello-plugin')
 Page({
   data: {
     items: [],
-    currentItem: 0
+    currentItem: 0,
   },
   onLoad() {
     plugin.sayHello()
     const world = plugin.answer
+    console.log(world)
   },
   addItem() {
     this.data.items.push(this.data.currentItem++)
     this.setData({
       items: this.data.items,
-      currentItem: this.data.currentItem
+      currentItem: this.data.currentItem,
     })
-  }
+  },
 })
