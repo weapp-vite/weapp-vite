8b052b9a9b300da98451b6f96c94927a4f16e597
ice breaker <1324318532@qq.com>
2025-12-31 22:55:04 +0800

    chore: upgrade

--- @weapp-core/schematics/test/schema.test.ts
+++ @weapp-core/schematics/test/schema.test.ts
@@ -1,7 +1,17 @@
+import fs from 'fs-extra'
+import path from 'pathe'
 import { buildSchemas } from '../scripts/utils'
 
 describe('schema', () => {
-  it('should work', async () => {
+  it('should generate deterministic schema JSON', async () => {
+    const schemaPath = path.resolve(__dirname, '../schemas/app.json')
+
     await buildSchemas()
+    const first = await fs.readFile(schemaPath, 'utf8')
+
+    await buildSchemas()
+    const second = await fs.readFile(schemaPath, 'utf8')
+
+    expect(second).toBe(first)
   })
 })
