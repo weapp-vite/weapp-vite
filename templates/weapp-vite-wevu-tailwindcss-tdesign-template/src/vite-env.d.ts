f9b24e730d09826493b83d6271fe7939230791c2
ice breaker <1324318532@qq.com>
2026-01-11 20:20:20 +0800

    chore: upgrade

--- templates/weapp-vite-wevu-tailwindcss-tdesign-template/src/vite-env.d.ts
+++ templates/weapp-vite-wevu-tailwindcss-tdesign-template/src/vite-env.d.ts
@@ -1,5 +1,4 @@
 /// <reference types="weapp-vite/client" />
-/// <reference types="@vue/runtime-core" />
 /// <reference types="wevu" />
 /// <reference path="../components.d.ts" />
 /// <reference path="../typed-components.d.ts" />
