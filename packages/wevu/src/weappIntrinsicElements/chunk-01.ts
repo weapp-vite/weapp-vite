8302ee203403d205c91ce0b2cec5269300aeb836
ice breaker <1324318532@qq.com>
2026-01-14 00:27:49 +0800

    chore: update

--- packages/wevu/src/weappIntrinsicElements/chunk-01.ts
+++
@@ -1,7 +0,0 @@
-// This file is auto-generated from components.json. Do not edit directly.
-
-import type { WeappIntrinsicElementsChunk01PartA } from './chunk-01.part-a'
-import type { WeappIntrinsicElementsChunk01PartB } from './chunk-01.part-b'
-
-export interface WeappIntrinsicElementsChunk01
-  extends WeappIntrinsicElementsChunk01PartA, WeappIntrinsicElementsChunk01PartB {}
