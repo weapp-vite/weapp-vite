1a325250568b420fb7404be3d3e5cdafec55ba12
ice breaker <1324318532@qq.com>
2026-01-16 17:09:55 +0800

    chore: add wevu-bug repo

---
+++ apps/wevu-bug/src/app.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { onHide, onLaunch, onShow } from 'wevu'
+
+defineAppJson({
+  pages: [
+    'pages/index/index',
+  ],
+  window: {
+    navigationBarTitleText: 'Wepp-Vite + WeVU Template',
+    navigationBarBackgroundColor: '#4c6ef5',
+    navigationBarTextStyle: 'white',
+  },
+  style: 'v2',
+  componentFramework: 'glass-easel',
+  sitemapLocation: 'sitemap.json',
+})
+
+onShow(() => {
+  console.log('[weapp-vite-wevu-template] app show')
+})
+
+onHide(() => {
+  console.log('[weapp-vite-wevu-template] app hide')
+})
+
+onLaunch(() => {
+  console.log('[weapp-vite-wevu-template] app launch')
+})
+</script>
+
+<style>
+page {
+  color: #1c1c3c;
+  background: #f6f7fb;
+}
+</style>
