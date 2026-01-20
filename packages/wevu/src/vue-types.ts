10ae1eaa25d4b64f028a0c2eccb8487d19aed5ef
ice breaker <1324318532@qq.com>
2026-01-20 15:53:33 +0800

    feat: 修复类型导出与内联逻辑，确保 wevu 类型文件不再依赖外部 `@vue/*` 包

---
+++ packages/wevu/src/vue-types.ts
@@ -0,0 +1,66 @@
+import type { Ref as VueRef, ShallowRef as VueShallowRef } from '@vue/reactivity'
+import type {
+  AllowedComponentProps as VueAllowedComponentProps,
+  ComponentCustomProps as VueComponentCustomProps,
+  ComponentOptionsMixin as VueComponentOptionsMixin,
+  ComputedOptions as VueComputedOptions,
+  DefineComponent as VueDefineComponent,
+  EmitsOptions as VueEmitsOptions,
+  MethodOptions as VueMethodOptions,
+  ObjectDirective as VueObjectDirective,
+  PublicProps as VuePublicProps,
+  ShallowUnwrapRef as VueShallowUnwrapRef,
+  SlotsType as VueSlotsType,
+  VNode as VueVNode,
+  VNodeProps as VueVNodeProps,
+} from '@vue/runtime-core'
+
+export type Ref<T = any, S = T> = VueRef<T, S>
+export type ShallowRef<T = any, S = T> = VueShallowRef<T, S>
+
+export type AllowedComponentProps = VueAllowedComponentProps
+export type ComponentCustomProps = VueComponentCustomProps
+export type ComponentOptionsMixin = VueComponentOptionsMixin
+export type DefineComponent<
+  PropsOrPropOptions = Record<string, any>,
+  RawBindings = Record<string, any>,
+  D = Record<string, any>,
+  C extends VueComputedOptions = VueComputedOptions,
+  M extends VueMethodOptions = VueMethodOptions,
+  Mixin extends VueComponentOptionsMixin = VueComponentOptionsMixin,
+  Extends extends VueComponentOptionsMixin = VueComponentOptionsMixin,
+  E extends VueEmitsOptions = VueEmitsOptions,
+  EE extends string = string,
+  PP = VuePublicProps,
+  Props = any,
+  Defaults = any,
+  S extends VueSlotsType = VueSlotsType,
+> = VueDefineComponent<
+  PropsOrPropOptions,
+  RawBindings,
+  D,
+  C,
+  M,
+  Mixin,
+  Extends,
+  E,
+  EE,
+  PP,
+  Props,
+  Defaults,
+  S
+>
+export type ObjectDirective<
+  HostElement = any,
+  Value = any,
+  Modifiers extends string = string,
+  Arg = any,
+> = VueObjectDirective<HostElement, Value, Modifiers, Arg>
+export type PublicProps = VuePublicProps
+export type ShallowUnwrapRef<T> = VueShallowUnwrapRef<T>
+export type VNode<HostNode = any, HostElement = any, ExtraProps = Record<string, any>> = VueVNode<
+  HostNode,
+  HostElement,
+  ExtraProps
+>
+export type VNodeProps = VueVNodeProps
