e07bb4a5593d8bfcf5606c343cba12b214c5c5d3
ice breaker <1324318532@qq.com>
2026-01-22 23:33:28 +0800

    chore: bump version

---
+++ .changeset/slimy-coins-win.md
@@ -0,0 +1,5 @@
+---
+"weapp-vite": patch
+---
+
+chore: 升级 rolldown -> 1.0.0-rc.1, vite -> 8.0.0-beta.9
--- packages/weapp-vite/test/tabbar-appbar.test.ts
+++ packages/weapp-vite/test/tabbar-appbar.test.ts
@@ -42,12 +42,12 @@ const jsExpectations: Record<string, Array<RegExp | string>> = {
   'pages/index/vue.js': [
     /require\(["']\.\.\/\.\.\/rolldown-runtime\.js["']\)/,
     /require\(["']\.\.\/\.\.\/common\.js["']\)/,
-    /require_common\.\w+\(\{/,
+    /require\(["']\.\.\/\.\.\/common\.js["']\)\.\w+\(\{/,
   ],
   'pages/index/vue-setup.js': [
     /require\(["']\.\.\/\.\.\/rolldown-runtime\.js["']\)/,
     /require\(["']\.\.\/\.\.\/common\.js["']\)/,
-    /require_common\.\w+\(\{/,
+    /require\(["']\.\.\/\.\.\/common\.js["']\)\.\w+\(\{/,
   ],
   'rolldown-runtime.js': [/Object\.defineProperty/],
 }
