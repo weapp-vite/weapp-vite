463f06ecaefadf1a6619e10856cc5f7f5d83e8e0
ice breaker <1324318532@qq.com>
2026-02-10 09:58:39 +0800

    chore(gitignore): ignore e2e lib-mode dist outputs

--- e2e-apps/lib-mode/dist-matrix/hoist-common/components/sfc-both/index.js
+++
@@ -1,8 +0,0 @@
-require("../../common.js").Dr(/* @__PURE__ */ Object.assign({ name: "SfcBoth" }, { setup(__props, { expose }) {
-	const __returned__ = { label: "script+setup" };
-	Object.defineProperty(__returned__, "__isScriptSetup", {
-		enumerable: false,
-		value: true
-	});
-	return __returned__;
-} }));
