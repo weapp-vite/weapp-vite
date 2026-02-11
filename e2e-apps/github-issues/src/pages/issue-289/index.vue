55f169cb835639eefdd6f1f199d4c11e491bd2ed
ice breaker <1324318532@qq.com>
2026-02-11 17:41:31 +0800

    test(e2e): add github-issues app with issue-289 regression

---
+++ e2e-apps/github-issues/src/pages/issue-289/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view class="page">
+    <InfoBanner :root="{ a: 'aaaa' }" />
+  </view>
+</template>
+
+<json>
+{
+  "component": false,
+  "usingComponents": {
+    "InfoBanner": "/components/InfoBanner/index"
+  }
+}
+</json>
