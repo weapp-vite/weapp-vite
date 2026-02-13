dc69e94eb885301beac0bc15bb1948e037c2fa3c
ice breaker <1324318532@qq.com>
2026-02-14 02:01:50 +0800

    refactor(web): extract core runtime data api wrappers

--- packages/web/src/runtime/polyfill/index.ts
+++ packages/web/src/runtime/polyfill/index.ts
@@ -6,52 +6,35 @@ import type {
   AppBaseInfo,
   AuthorizeOptions,
   BatteryInfo,
-  CanvasContext,
   CheckSessionOptions,
   CloudBridge,
   DeviceInfo,
-  DownloadFileOptions,
-  FileSystemManager,
   GetBatteryInfoSuccessResult,
   GetExtConfigOptions,
   GetFuzzyLocationOptions,
   GetLocationOptions,
   GetNetworkTypeOptions,
   GetSettingOptions,
-  GetStorageOptions,
   GetSystemInfoOptions,
   GetUserInfoOptions,
   GetUserProfileOptions,
   InterstitialAd,
-  LoadSubPackageOptions,
   LoginOptions,
   LogManager,
   LogManagerOptions,
   MenuButtonBoundingClientRect,
-  NavigateToMiniProgramOptions,
   NetworkStatusChangeCallback,
   OpenAppAuthorizeSettingOptions,
   OpenSettingOptions,
-  PageScrollToOptions,
-  PreloadSubpackageOptions,
-  RemoveStorageOptions,
-  RequestOptions,
   RewardedVideoAd,
-  SelectorQuery,
   SetBackgroundColorOptions,
   SetBackgroundTextStyleOptions,
-  SetStorageOptions,
-  StorageInfoResult,
   SystemInfo,
   SystemSetting,
   UpdateManager,
-  UploadFileOptions,
   VibrateShortOptions,
-  VideoContext,
-  VkSession,
   WindowInfo,
   WindowResizeCallback,
-  WorkerBridge,
   WxAsyncOptions,
   WxBaseResult,
 } from './types'
@@ -76,7 +59,6 @@ import {
   setBackgroundColorBridge,
   setBackgroundTextStyleBridge,
 } from './background'
-import { createCanvasContextBridge } from './canvasContext'
 import { createCloudBridge } from './cloud'
 import {
   getBatteryInfoBridge,
@@ -92,11 +74,6 @@ import {
   getNetworkTypeBridge,
 } from './menuApi'
 import { createNavigationBarRuntimeBridge } from './navigationBarRuntime'
-import {
-  downloadFileByFetchBridge,
-  requestByFetchBridge,
-  uploadFileByFetchBridge,
-} from './network'
 import {
   createInterstitialAdBridge,
   createRewardedVideoAdBridge,
@@ -125,35 +102,36 @@ import {
   onWindowResizeBridge,
 } from './runtimeCapabilityApi'
 import {
-  clearStorageSyncBridge,
-  createFileSystemManagerBridgeApi,
-  createVKSessionBridgeApi,
-  createWorkerBridgeApi,
-  getStorageInfoSyncBridge,
-  getStorageSyncBridge,
-  removeStorageSyncBridge,
-  setStorageSyncBridge,
-} from './runtimeInfra'
-import {
-  hideKeyboardBridge,
-  loadSubPackageBridge,
-  nextTickBridge,
-  pageScrollToBridge,
-  preloadSubpackageBridge,
-  startPullDownRefreshBridge,
-  stopPullDownRefreshBridge,
-} from './runtimeOps'
-import { createSelectorQueryBridge } from './selectorQuery'
-import {
-  clearStorageBridge,
-  getStorageBridge,
-  getStorageInfoBridge,
-  removeStorageBridge,
-  setStorageBridge,
-} from './storageAsync'
-import {
-  resolveDeviceOrientation,
-} from './system'
+  clearStorage,
+  clearStorageSync,
+  createCanvasContext,
+  createSelectorQuery,
+  createVideoContext,
+  createVKSession,
+  createWorker,
+  downloadFile,
+  exitMiniProgram,
+  getFileSystemManager,
+  getStorage,
+  getStorageInfo,
+  getStorageInfoSync,
+  getStorageSync,
+  hideKeyboard,
+  loadSubPackage,
+  navigateToMiniProgram,
+  nextTick,
+  pageScrollTo,
+  preloadSubpackage,
+  removeStorage,
+  removeStorageSync,
+  request,
+  setStorage,
+  setStorageSync,
+  startPullDownRefresh,
+  stopPullDownRefresh,
+  uploadFile,
+} from './runtimeDataApi'
+import { resolveDeviceOrientation } from './system'
 import {
   getAccountInfoSyncBridge,
   getAppBaseInfoBridge,
@@ -202,8 +180,6 @@ import {
   updateShareMenu,
 } from './uiMediaApi'
 
-import { createVideoContextBridge } from './videoContext'
-
 export {
   getEnterOptionsSync,
   getLaunchOptionsSync,
@@ -217,6 +193,37 @@ export {
   reLaunch,
   switchTab,
 } from './routeRuntime'
+
+export {
+  clearStorage,
+  clearStorageSync,
+  createCanvasContext,
+  createSelectorQuery,
+  createVideoContext,
+  createVKSession,
+  createWorker,
+  downloadFile,
+  exitMiniProgram,
+  getFileSystemManager,
+  getStorage,
+  getStorageInfo,
+  getStorageInfoSync,
+  getStorageSync,
+  hideKeyboard,
+  loadSubPackage,
+  navigateToMiniProgram,
+  nextTick,
+  pageScrollTo,
+  preloadSubpackage,
+  removeStorage,
+  removeStorageSync,
+  request,
+  setStorage,
+  setStorageSync,
+  startPullDownRefresh,
+  stopPullDownRefresh,
+  uploadFile,
+} from './runtimeDataApi'
 
 export {
   chooseAddress,
@@ -256,59 +263,6 @@ export {
   showToast,
   updateShareMenu,
 } from './uiMediaApi'
-
-export function navigateToMiniProgram(options?: NavigateToMiniProgramOptions) {
-  const appId = options?.appId?.trim() ?? ''
-  if (!appId) {
-    const failure = callWxAsyncFailure(options, 'navigateToMiniProgram:fail invalid appId')
-    return Promise.reject(failure)
-  }
-  return Promise.resolve(callWxAsyncSuccess(options, { errMsg: 'navigateToMiniProgram:ok' }))
-}
-
-export function exitMiniProgram(options?: WxAsyncOptions<WxBaseResult>) {
-  return Promise.resolve(callWxAsyncSuccess(options, { errMsg: 'exitMiniProgram:ok' }))
-}
-
-export function nextTick(callback?: () => void) {
-  return nextTickBridge(callback)
-}
-
-export function startPullDownRefresh(options?: WxAsyncOptions<WxBaseResult>) {
-  return startPullDownRefreshBridge(options)
-}
-
-export function stopPullDownRefresh(options?: WxAsyncOptions<WxBaseResult>) {
-  return stopPullDownRefreshBridge(options)
-}
-
-export function hideKeyboard(options?: WxAsyncOptions<WxBaseResult>) {
-  return hideKeyboardBridge(options)
-}
-
-export function loadSubPackage(options?: LoadSubPackageOptions) {
-  return loadSubPackageBridge(options)
-}
-
-export function preloadSubpackage(options?: PreloadSubpackageOptions) {
-  return preloadSubpackageBridge(options)
-}
-
-export function pageScrollTo(options?: PageScrollToOptions) {
-  return pageScrollToBridge(options)
-}
-
-export function createSelectorQuery(): SelectorQuery {
-  return createSelectorQueryBridge()
-}
-
-export function createCanvasContext(canvasId: string): CanvasContext {
-  return createCanvasContextBridge(canvasId)
-}
-
-export function createVideoContext(videoId: string): VideoContext {
-  return createVideoContextBridge(videoId)
-}
 
 const WEB_SUPPORTED_AUTH_SCOPES = new Set([
   'scope.userInfo',
@@ -364,81 +318,6 @@ export function setBackgroundColor(options?: SetBackgroundColorOptions) {
 
 export function setBackgroundTextStyle(options?: SetBackgroundTextStyleOptions) {
   return setBackgroundTextStyleBridge(options)
-}
-
-export function setStorageSync(key: string, data: any) {
-  return setStorageSyncBridge(key, data)
-}
-
-export function getStorageSync(key: string) {
-  return getStorageSyncBridge(key)
-}
-
-export function removeStorageSync(key: string) {
-  return removeStorageSyncBridge(key)
-}
-
-export function clearStorageSync() {
-  return clearStorageSyncBridge()
-}
-
-export function getStorageInfoSync(): StorageInfoResult {
-  return getStorageInfoSyncBridge() as StorageInfoResult
-}
-
-export function setStorage(options?: SetStorageOptions) {
-  return setStorageBridge(options)
-}
-
-export function getStorage(options?: GetStorageOptions) {
-  return getStorageBridge(options)
-}
-
-export function removeStorage(options?: RemoveStorageOptions) {
-  return removeStorageBridge(options)
-}
-
-export function clearStorage(options?: WxAsyncOptions<WxBaseResult>) {
-  return clearStorageBridge(options)
-}
-
-export function getStorageInfo(options?: WxAsyncOptions<StorageInfoResult>) {
-  return getStorageInfoBridge(options)
-}
-
-const fileSystemManagerBridge: FileSystemManager = createFileSystemManagerBridgeApi(
-  (options: any, result: any) => callWxAsyncSuccess(
-    options as unknown as WxAsyncOptions<WxBaseResult> | undefined,
-    result as WxBaseResult,
-  ),
-  (options: any, errMsg: any) => callWxAsyncFailure(
-    options as unknown as WxAsyncOptions<WxBaseResult> | undefined,
-    errMsg,
-  ),
-) as FileSystemManager
-
-export function getFileSystemManager() {
-  return fileSystemManagerBridge
-}
-
-export function createWorker(path: string): WorkerBridge {
-  return createWorkerBridgeApi(path) as WorkerBridge
-}
-
-export function createVKSession(_options?: Record<string, unknown>): VkSession {
-  return createVKSessionBridgeApi() as VkSession
-}
-
-export async function request(options?: RequestOptions) {
-  return requestByFetchBridge(options)
-}
-
-export async function downloadFile(options?: DownloadFileOptions) {
-  return downloadFileByFetchBridge(options)
-}
-
-export async function uploadFile(options?: UploadFileOptions) {
-  return uploadFileByFetchBridge(options)
 }
 
 export function vibrateShort(options?: VibrateShortOptions) {
