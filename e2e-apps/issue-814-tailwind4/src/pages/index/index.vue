9fb9105e8994c5a5d5529bdbaeeaa547f960b661
ice breaker <1324318532@qq.com>
2026-02-20 01:58:26 +0800

    test(e2e): cover issue-814 dynamic class arbitrary values

--- e2e-apps/issue-814-tailwind4/src/pages/index/index.vue
+++ e2e-apps/issue-814-tailwind4/src/pages/index/index.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'wevu'
+
 definePageJson({
   navigationBarTitleText: 'Issue 814 Tailwind4',
 })
+
+const bbb = ref('-row')
+const aaa = ref('items-center')
 </script>
 
 <template>
-  <view class="flex gap-[20px]">
+  <view class="flex gap-[24px]">
     <view>1</view>
     <view>2</view>
     <view>3</view>
   </view>
-  <view class="flex gap-[20px]">
+  <view :class="[`flex${bbb} gap-[17px]`, aaa]">
     <view>1</view>
     <view>2</view>
     <view>3</view>
