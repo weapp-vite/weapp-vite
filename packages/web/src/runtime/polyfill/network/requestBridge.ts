16a35db2293493b7a2c9101848b31be0e3997a72
ice breaker <1324318532@qq.com>
2026-03-08 17:30:02 +0800

    refactor(web): split oversized runtime modules

---
+++ packages/web/src/runtime/polyfill/network/requestBridge.ts
@@ -0,0 +1,56 @@
+import {
+  callWxAsyncFailure,
+  callWxAsyncSuccess,
+} from '../async'
+import {
+  performDownloadByFetch,
+  performRequestByFetch,
+  performUploadByFetch,
+} from './request'
+
+export async function requestByFetchBridge(options?: RequestOptions) {
+  try {
+    const response = await performRequestByFetch(options)
+    return callWxAsyncSuccess(options, {
+      errMsg: 'request:ok',
+      data: response.data,
+      statusCode: response.statusCode,
+      header: response.header,
+    })
+  }
+  catch (error) {
+    const message = error instanceof Error ? error.message : String(error)
+    return Promise.reject(callWxAsyncFailure(options, `request:fail ${message}`))
+  }
+}
+
+export async function downloadFileByFetchBridge(options?: DownloadFileOptions) {
+  try {
+    const response = await performDownloadByFetch(options)
+    return callWxAsyncSuccess(options, {
+      errMsg: 'downloadFile:ok',
+      tempFilePath: response.tempFilePath,
+      statusCode: response.statusCode,
+    })
+  }
+  catch (error) {
+    const message = error instanceof Error ? error.message : String(error)
+    return Promise.reject(callWxAsyncFailure(options, `downloadFile:fail ${message}`))
+  }
+}
+
+export async function uploadFileByFetchBridge(options?: UploadFileOptions) {
+  try {
+    const response = await performUploadByFetch(options)
+    return callWxAsyncSuccess(options, {
+      errMsg: 'uploadFile:ok',
+      data: response.data,
+      statusCode: response.statusCode,
+      header: response.header,
+    })
+  }
+  catch (error) {
+    const message = error instanceof Error ? error.message : String(error)
+    return Promise.reject(callWxAsyncFailure(options, `uploadFile:fail ${message}`))
+  }
+}
