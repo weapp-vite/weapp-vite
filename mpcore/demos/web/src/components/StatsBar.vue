d1a65aba5d30f65b3236414e7b0e94aa4e5863db
ice breaker <1324318532@qq.com>
2026-03-26 11:54:37 +0800

    refactor(mpcore): migrate simulator demo to vue sfc

---
+++ mpcore/demos/web/src/components/StatsBar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  items: Array<{
+    label: string
+    value: string | number
+  }>
+}>()
+</script>
+
+<template>
+  <div class="sim-stats">
+    <article v-for="item in items" :key="item.label" class="sim-stat">
+      <span>{{ item.label }}</span>
+      <strong>{{ item.value }}</strong>
+    </article>
+  </div>
+</template>
