aa87dc06802b5c3861a958660aaab750be07e160
ice breaker <1324318532@qq.com>
2026-04-03 14:10:47 +0800

    fix(weapp-vite): avoid node_modules path chunks for dev deps

---
+++ e2e-apps/github-issues/src/pages/issue-393/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import debounce from 'debounce'
+import DebounceProbe from '../../components/issue-393/DebounceProbe/index.vue'
+
+definePageJson({
+  navigationBarTitleText: 'issue-393',
+  backgroundColor: '#ffffff',
+})
+
+const issue393Marker = typeof debounce === 'function'
+  ? 'debounce-loaded'
+  : 'debounce-missing'
+
+function _runE2E() {
+  return {
+    marker: issue393Marker,
+  }
+}
+</script>
+
+<template>
+  <view class="issue-393-page">
+    <text>issue-393 sharedMode path devDependency import</text>
+    <text>{{ issue393Marker }}</text>
+    <DebounceProbe />
+  </view>
+</template>
