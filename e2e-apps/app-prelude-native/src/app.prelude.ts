69d66aeabcb68887c651283fabc2ff18bf5acc3e
ice breaker <1324318532@qq.com>
2026-04-11 12:16:43 +0800

    test(e2e): log app prelude execution

--- e2e-apps/app-prelude-native/src/app.prelude.ts
+++ e2e-apps/app-prelude-native/src/app.prelude.ts
@@ -2,7 +2,12 @@ const host = globalThis as typeof globalThis & {
   __appPreludeLog__?: string[]
 }
 
+const message = `app.prelude.ts:${import.meta.filename}`
+
+// eslint-disable-next-line no-console
+console.log(message)
+
 host.__appPreludeLog__ = [
   ...(host.__appPreludeLog__ ?? []),
-  `app.prelude.ts:${import.meta.filename}`,
+  message,
 ]
