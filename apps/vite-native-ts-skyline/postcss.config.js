cc1839d65e28a83bbb4fbed7b470b161de05da9c
ice breaker <1324318532@qq.com>
2026-04-27 22:30:55 +0800

    fix(apps): update tailwind postcss plugins

--- apps/vite-native-ts-skyline/postcss.config.js
+++ apps/vite-native-ts-skyline/postcss.config.js
@@ -1,7 +1,7 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     // 假如框架已经内置了 `autoprefixer`，可以去除下一行
-    autoprefixer: {},
+    'autoprefixer': {},
   },
 }
