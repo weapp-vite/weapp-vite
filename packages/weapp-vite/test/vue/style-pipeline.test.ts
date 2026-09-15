f0a7843e1e59caf98c050574eaa4e02b18cb13fe
daguanren21 <1363917353@qq.com>
2026-09-15 11:43:07 +0800

    test(weapp-vite): initialize complete runtime state in Vue fixtures

--- packages/weapp-vite/test/vue/style-pipeline.test.ts
+++ packages/weapp-vite/test/vue/style-pipeline.test.ts
@@ -7,17 +7,13 @@ import { createVueResolverPlugin } from '../../src/plugins/vue/resolver'
 import { createVueTransformPlugin } from '../../src/plugins/vue/transform'
 import { WEAPP_VUE_STYLE_VIRTUAL_PREFIX } from '../../src/plugins/vue/transform/styleRequest'
 import { callPluginHook } from '../pluginHook'
-import { createTestModuleGraphService } from './moduleGraph'
+import { createTestModuleGraphService, createTestRuntimeState } from './moduleGraph'
 
 function createCtx(root: string) {
   const absoluteSrcRoot = path.join(root, 'src')
   return {
     moduleGraphService: createTestModuleGraphService(),
-    runtimeState: {
-      scan: {
-        isDirty: false,
-      },
-    },
+    runtimeState: createTestRuntimeState(),
     configService: {
       cwd: root,
       absoluteSrcRoot,
