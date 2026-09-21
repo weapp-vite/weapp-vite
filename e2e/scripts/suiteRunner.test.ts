054b3e235f6139ed417e33db5f09c1bb4e469e13
ice breaker <1324318532@qq.com>
2026-09-21 16:20:27 +0800

    test(e2e): include issue 1049 in suite runner contract

--- e2e/scripts/suiteRunner.test.ts
+++ e2e/scripts/suiteRunner.test.ts
@@ -637,6 +637,7 @@ describe('suiteRunner', () => {
       'ide/github-issues.runtime.issue911.test.ts',
       'ide/github-issues.runtime.issue941.test.ts',
       'ide/github-issues.runtime.issue1009.test.ts',
+      'ide/github-issues.runtime.issue1049.test.ts',
       'ide/github-issues.runtime.issue1011.test.ts',
       'ide/github-issues.runtime.issue1012.test.ts',
       'ide/github-issues.runtime.issue852.test.ts',
